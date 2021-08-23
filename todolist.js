ba6c048be260bbde084842c143bf8982cbec4c52
MiladNz <milad.n91@gmail.com>
2021-08-24 00:19:53 +0430

    commit on master

--- todolist.js
+++ todolist.js
@@ -1,11 +1,14 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-btn");
 const todoList = document.querySelector(".todolist");
+
 todoButton.addEventListener("click", addTodo);
+
+todoList.addEventListener("click", checkRemove);
 
 function addTodo(e){
     e.preventDefault();
-    console.log(e);
+    //console.log(e);
 
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
@@ -16,4 +19,17 @@ function addTodo(e){
     todoDiv.innerHTML = newTodo;
     todoList.appendChild(todoDiv);
     todoInput.value = "";
+}
+
+function checkRemove(e) {
+    // console.log(e.target.classList.value);
+    const classList =[...e.target.classList]; //convert DomToken to array
+    const item = e.target;
+    if (classList[1] === "fa-check-square"){
+        const todo = item.parentElement.parentElement;
+        todo.classList.toggle("completed");
+    } else if(classList[1] === "fa-trash-alt"){
+        const todo = item.parentElement.parentElement;
+        todo.remove();
+    }
 }
