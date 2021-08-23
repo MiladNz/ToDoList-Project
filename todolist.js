70237438ec094258c43fcbde74eedd29522665d1
MiladNz <milad.n91@gmail.com>
2021-08-23 20:44:44 +0430

    commit on master

--- todolist.js
+++ todolist.js
@@ -1,1 +1,19 @@
-//js
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-btn");
+const todoList = document.querySelector(".todolist");
+todoButton.addEventListener("click", addTodo);
+
+function addTodo(e){
+    e.preventDefault();
+    console.log(e);
+
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    const newTodo =
+    `<li>${todoInput.value}</li>
+    <span><i class="far fa-check-square"></i></span>
+    <span><i class="far fa-trash-alt"></i></span>`;
+    todoDiv.innerHTML = newTodo;
+    todoList.appendChild(todoDiv);
+    todoInput.value = "";
+}
